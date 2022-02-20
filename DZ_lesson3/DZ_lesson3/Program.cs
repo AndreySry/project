5b2d2631083e180085c35c6e44e67a9a68c68492
AndreySry <matwey059@gmail.com>
2022-02-21 04:48:47 +0700

    ДЗ третьего урока

---
+++ DZ_lesson3/DZ_lesson3/Program.cs
@@ -0,0 +1,82 @@
+﻿/*Выполнил: Андрей Срыбный
+ * Задание:
+ * 1 в) Добавить диалог с использованием switch демонстрирующий работу класса.
+ */
+using System;
+namespace DZ_lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Описали ссылку на объект.
+            Complex complex1;
+            // Создали объект и сохранили ссылку на него в complex1.
+            complex1 = new Complex(1, 1);
+            // Описали объект и создали его.
+            Complex complex2 = new Complex(2, 2);
+            // С помощью свойства Im изменили внутреннее (приватное) поле im.
+            complex2.Im = 3;
+            // Создали ссылку на объект.
+            Complex result;
+            static void Pause()
+            {
+                Console.WriteLine("press any key");
+                Console.ReadKey();
+            }
+
+
+            int taskNumber;
+            do
+            {
+                Console.WriteLine("для просмотра сложения комплексных чисел - введите с клавиатуры цифру 1");
+                Console.WriteLine("для просмотра вычитания комплексных чисел - введите 2");
+                Console.WriteLine("для просмотра умножение комплексных чисел - введите 3");
+                Console.WriteLine("для просмотра деление комплексных чисел - введите 4");
+                Console.WriteLine("для завершения просмотра  - введите 0");
+                taskNumber = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (taskNumber)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        Console.WriteLine($"Первое комплексное число: {complex1.ToString()}");
+                        Console.WriteLine($"Второе комплексное число: {complex2.ToString()}");
+                        result = complex1.Plus(complex2);
+                        Console.WriteLine($"Результат сложения: {result.ToString()}");
+                        Pause();
+                        break;
+                    case 2:
+                        Console.WriteLine($"Первое комплексное число: {complex1.ToString()}");
+                        Console.WriteLine($"Второе комплексное число: {complex2.ToString()}");
+                        result = complex1.Minus(complex2);
+                        Console.WriteLine($"Результат вычитания: {result.ToString()}");
+                        Pause();
+                        break;
+                    case 3:
+                        Console.WriteLine($"Первое комплексное число: {complex1.ToString()}");
+                        Console.WriteLine($"Второе комплексное число: {complex2.ToString()}");
+                        result = complex1.Multiplication(complex2);
+                        Console.WriteLine($"Результат умножения: {result.ToString()}");
+                        Pause();
+                        break;
+                    case 4:
+                        Console.WriteLine($"Первое комплексное число: {complex1.ToString()}");
+                        Console.WriteLine($"Второе комплексное число: {complex2.ToString()}");
+                        result = complex1.Division(complex2);
+                        Console.WriteLine($"Результат деления: {result.ToString()}");
+                        Pause();
+                        break;
+                    
+                    default:
+                        break;
+                }
+                Console.Clear();
+
+            } while (taskNumber != 0);
+        }
+    }
+
+}
+
---
+++ DZ_lesson3/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Выполнил Андрей Срыбный
+ *   Задание:
+ *   3.* Описать класс дробей — рациональных чисел, являющихся отношением двух целых чисел. 
+ *       Предусмотреть методы сложения, вычитания, умножения и деления дробей. Написать программу, демонстрирующую все разработанные элементы класса.
+ *    *    Добавить свойства типа int для доступа к числителю и знаменателю;
+ *    *    Добавить свойство типа double только на чтение, чтобы получить десятичную дробь числа;
+ *    **   Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение ArgumentException("Знаменатель не может быть равен 0");
+ *    ***  Добавить упрощение дробей.
+ */
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Основная программа
+            OrdinaryFraction a = new OrdinaryFraction(4, -8);//создание объекта класса OrdinaryFraction
+            OrdinaryFraction b = new OrdinaryFraction(2, 5);//создание объекта класса OrdinaryFraction
+
+            OrdinaryFraction sum;
+            sum = a + b;
+            OrdinaryFraction subtraction;
+            subtraction = a - b;
+            OrdinaryFraction multiplication;
+            multiplication = a * b;
+            OrdinaryFraction division;
+            division = a / b;
+            Console.WriteLine("Проверка на сложение: " + a.ToString() + "+" + b.ToString() + "=" + sum.ToString());
+            Console.WriteLine("Проверка на вычитание: " + a.ToString() + "-" + b.ToString() + "=" + subtraction.ToString());
+            Console.WriteLine("Проверка на умножение: " + a.ToString() + "*" + b.ToString() + "=" + multiplication.ToString());
+            Console.WriteLine("Проверка на деление: " + a.ToString() + "/" + b.ToString() + "=" + division.ToString());
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
